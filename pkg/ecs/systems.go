e472234a63503c8af3d2c63c7c788488ebc899a4
rangelsen <roy.angelsen@gmail.com>
2023-05-01 00:30:53 +0200

    Test entity id allocator

--- pkg/ecs/systems.go
+++ pkg/ecs/systems.go
@@ -3,7 +3,16 @@ package ecs
 import ()
 
 type (
-	EntityID        int
+	EntityID struct {
+		index   uint32
+		version uint32
+	}
+
+	AllocatorEntry struct {
+		isActive bool
+		version  uint32
+	}
+
 	ComponentMask   uint32
 	ComponentID     ComponentMask
 	ComponentMatrix []ComponentMask
@@ -21,9 +30,15 @@ type (
 	}
 
 	EntityComponents struct {
-		nEntities   uint32
-		nComponents uint32
+		nEntities   uint
+		nComponents uint
 		matrix      ComponentMatrix
+		allocator   EntityIDAllocator
+	}
+
+	EntityIDAllocator struct {
+		entities []AllocatorEntry
+		free     []uint32
 	}
 
 	MeshComponent struct {
@@ -39,7 +54,7 @@ type (
 	}
 
 	System interface {
-		update(dt float32, entities []EntityID, compRepo *ComponentRepo)
+		update(dt float32, entities []uint32, compRepo *ComponentRepo)
 		mask() ComponentMask
 	}
 
@@ -49,8 +64,9 @@ type (
 )
 
 const (
+	MAX_COMPONENTS uint = 32
 
-	// If exceeds 32 different entities, change the type of ComponentMask
+	// If exceeds 32 different components, change the type of ComponentMask
 	TF_COMPID ComponentID = iota + 1
 	VELOCITY_COMPID
 	RENDER_COMPID
@@ -70,7 +86,7 @@ func (scene *Scene) Update(dt float32) {
 	}
 }
 
-func (sys MovementSystem) update(dt float32, entities []EntityID,
+func (sys MovementSystem) update(dt float32, entities []uint32,
 	compRepo *ComponentRepo) {
 }
 
@@ -78,11 +94,11 @@ func (sys MovementSystem) mask() ComponentMask {
 	return sys.components
 }
 
-func (scene *Scene) filterEntities(mask ComponentMask) []EntityID {
-	var entities []EntityID
+func (scene *Scene) filterEntities(mask ComponentMask) []uint32 {
+	var entities []uint32
 	for i, entityMask := range scene.entities.matrix {
 		if entityMask == mask {
-			entities = append(entities, EntityID(i))
+			entities = append(entities, uint32(i))
 		}
 	}
 	return entities
@@ -91,7 +107,99 @@ func (scene *Scene) filterEntities(mask ComponentMask) []EntityID {
 func makeComponentMask(comps ...ComponentID) ComponentMask {
 	var mask ComponentMask
 	for _, c := range comps {
-		mask = mask | ComponentMask(c)
+		mask |= ComponentMask(c)
 	}
 	return mask
+}
+
+func NewScene(nEntities uint) *Scene {
+	return &Scene{
+		entities:   newEntities(nEntities),
+		components: newComponents(nEntities),
+	}
+}
+
+func newEntities(nEntities uint) EntityComponents {
+	return EntityComponents{
+		nEntities:   nEntities,
+		nComponents: MAX_COMPONENTS,
+		matrix:      newComponentMatrix(nEntities, MAX_COMPONENTS),
+		allocator:   newEntityAllocator(nEntities),
+	}
+}
+
+func newComponents(nEnts uint) ComponentRepo {
+	return ComponentRepo{
+		meshComps:   make([]MeshComponent, nEnts),
+		renderComps: make([]RenderComponent, nEnts),
+		tfComps:     make([]TransformComponent, nEnts),
+		velComps:    make([]VelocityComponent, nEnts),
+	}
+}
+
+func newComponentMatrix(nEnts uint, nComps uint) ComponentMatrix {
+	var mat ComponentMatrix = make([]ComponentMask, nEnts)
+	for i := range mat {
+		mat[i] = ComponentMask(0)
+	}
+	return mat
+}
+
+func newEntityAllocator(n uint) EntityIDAllocator {
+	alloc := EntityIDAllocator{
+		entities: make([]AllocatorEntry, n),
+		free:     make([]uint32, n),
+	}
+	for i := range alloc.free {
+		alloc.free[i] = uint32(i)
+	}
+	return alloc
+}
+
+func (scene *Scene) NewEntity() EntityID {
+	return scene.entities.allocator.allocate()
+}
+
+func (alloc *EntityIDAllocator) allocate() EntityID {
+	if len(alloc.free) > 0 {
+		var entityIndex uint32
+		entityIndex, alloc.free = alloc.free[0], alloc.free[1:]
+		alloc.entities[entityIndex].version++
+		alloc.entities[entityIndex].isActive = true
+		return EntityID{
+			index:   entityIndex,
+			version: alloc.entities[entityIndex].version,
+		}
+	}
+	entry := AllocatorEntry{
+		isActive: true,
+		version:  0,
+	}
+	alloc.entities = append(alloc.entities, entry)
+	return EntityID{
+		index:   uint32(len(alloc.entities) - 1),
+		version: 0,
+	}
+}
+
+func (alloc *EntityIDAllocator) deallocate(entity EntityID) {
+	if alloc.isActive(entity) {
+		alloc.entities[entity.index].isActive = false
+		alloc.free = append(alloc.free, entity.index)
+	}
+}
+
+func (alloc *EntityIDAllocator) isActive(entity EntityID) bool {
+	return entity.index < uint32(len(alloc.entities)) &&
+		alloc.entities[entity.index].version == entity.version &&
+		alloc.entities[entity.index].isActive
+}
+
+func contains[T comparable](xs []T, x T) bool {
+	for _, val := range xs {
+		if val == x {
+			return true
+		}
+	}
+	return false
 }
--- pkg/ecs/systems_test.go
+++ pkg/ecs/systems_test.go
@@ -1,16 +1,129 @@
 package ecs
 
 import (
-    "testing"
+	"testing"
 )
 
 func TestMakeComponentMask(t *testing.T) {
-    componentA := ComponentID(2)  // binary: 0010
-    componentB := ComponentID(8)  // binary: 1000
-    expected := ComponentMask(10) // binary: 1010
+    componentA := ComponentID(3)  // binary: 0011
+    componentB := ComponentID(10) // binary: 1010
+    expected := ComponentMask(11) // binary: 1011
     actual := makeComponentMask(componentA, componentB)
 
     if expected != actual {
         t.Errorf("expected: %d, actual: %d", expected, actual)
+    }
+}
+
+func TestNewComponentMatrix_ShouldAddEntitiesAndComponents(t *testing.T) {
+    // Arrange
+    nEntities, nComponents := uint(3), uint(16)
+
+    // Act
+    mat := newComponentMatrix(nEntities, nComponents)
+
+    // Assert
+    rows := len(mat)
+    cols := mat[0]
+    if uint(rows) != nEntities {
+        t.Errorf("expected number of rows to be %d", nEntities)
+    }
+    if cols != 0 {
+        t.Errorf("expected component mask to be 0, was %d", cols)
+    }
+}
+
+func TestEntityComponents_newEntityComponents_ShouldAddEntities(t *testing.T) {
+    maxEntities := uint(3)
+    entities := newEntities(maxEntities)
+    if entities.nEntities != maxEntities {
+        t.Errorf("expected %d number of entities, was %d", maxEntities, entities.nEntities)
+    }
+    lenAllocEntities := len(entities.allocator.entities)
+    lenAllocFree := len(entities.allocator.free)
+    if lenAllocEntities != lenAllocFree || uint(lenAllocEntities) != maxEntities {
+        t.Errorf("number of allocator entities was not equal to number of free indices, was no equal to %d", maxEntities)
+    }
+    lenCompMat := len(entities.matrix)
+    if uint(lenCompMat) != maxEntities {
+        t.Errorf("expected len comp matrix to be %d", maxEntities)
+    }
+}
+
+func TestScene_NewScene_ShouldCreateEntities(t *testing.T) {
+    maxEntities := uint(3)
+    scene := NewScene(maxEntities)
+    if len(scene.components.meshComps) != int(maxEntities) {
+        t.Errorf("expected %d number of mesh components, was %d", maxEntities, len(scene.components.meshComps))
+    }
+    if scene.entities.nEntities != maxEntities {
+        t.Errorf("expected %d number of entities, was %d", maxEntities, scene.entities.nEntities)
+    }
+    lenAllocEntities := len(scene.entities.allocator.entities)
+    lenAllocFree := len(scene.entities.allocator.free)
+    if lenAllocEntities != lenAllocFree || uint(lenAllocEntities) != maxEntities {
+        t.Errorf("number of allocator entities was not equal to number of free indices, was no equal to %d", maxEntities)
+    }
+    lenCompMat := len(scene.entities.matrix)
+    if uint(lenCompMat) != maxEntities {
+        t.Errorf("expected len comp matrix to be %d", maxEntities)
+    }
+}
+
+func TestScene_AddEntity_ShouldBe0(t *testing.T) {
+    maxEntities := uint(3)
+    scene := NewScene(maxEntities)
+    entity := scene.NewEntity()
+    actual := EntityID{0, 1}
+    if entity != actual {
+        t.Errorf("expected allocated entity to be %v, was %v", entity, actual)
+    }
+}
+
+func TestContains(t *testing.T) {
+    entities := []uint32{42, 2, 8}
+    if !contains(entities, 42) {
+        t.Errorf("expected 42 to be in %v", entities)
+    }
+    if contains(entities, 82) {
+        t.Errorf("Did not expect to find 82 in %v", entities)
+    }
+}
+
+func TestEntityIDAllocator_SingleAllocation_ShouldReturn0(t *testing.T) {
+    alloc := newEntityAllocator(10)
+    actual := alloc.allocate()
+    expected := EntityID{index: 0, version: 1}
+    if expected != actual {
+        t.Errorf("expected %v, actual %v", expected, actual)
+    }
+}
+
+func TestEntityIDAllocator_TwoAllocations_ShouldIncrementIndex(t *testing.T) {
+    alloc := newEntityAllocator(10)
+    alloc.allocate()
+    actual := alloc.allocate()
+    expected := EntityID{index: 1, version: 1}
+    if expected != actual {
+        t.Errorf("expected %v, actual %v", expected, actual)
+    }
+}
+
+func TestEntityIDAllocator_Free_ShouldIncreaseVersionAndMoveToFree(t *testing.T) {
+    alloc := newEntityAllocator(10)
+    entity := alloc.allocate()
+    if !alloc.entities[entity.index].isActive {
+        t.Errorf("expected entity to be active, was %v", alloc.entities[entity.index])
+    }
+    alloc.deallocate(entity)
+
+    if alloc.entities[0].version != 1 {
+        t.Error("expected entity version 0")
+    }
+    if len(alloc.free) != 10 {
+        t.Errorf("expected 10 free indices, have %d", len(alloc.free))
+    }
+    if alloc.entities[entity.index].isActive {
+        t.Errorf("expected entity %v to be inactive, was %v", entity, alloc.entities[entity.index])
     }
 }
